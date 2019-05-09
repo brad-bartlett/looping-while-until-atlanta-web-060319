bda7c5d6b2039ee8efe2a33a25d6c2893f534d90
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-09 15:39:28 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,12 @@
 def using_while
   levitation_force = 6
+end
 
-  while levitation_force < 10 do
+while levitation_force < 10 do
 
     puts "Wingardian Leviosa"
     levitation_force += 1
 
 
-  end
-
 end
+
